d20f3731a5b992f191114c5e19c8327132101e93
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-03-09 17:49:51 +0200

    add libgdx

---
+++ desktop/src/fi/tuni/tamk/tiko/depressionaut/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.tuni.tamk.tiko.depressionaut.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tuni.tamk.tiko.depressionaut.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
