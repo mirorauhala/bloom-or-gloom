2dbaa95c8add9286c0e098ae8a2889a4db340baa
jerejoensuu <jere.joensuu@tuni.fi>
2021-03-19 20:25:38 +0200

    Add basic functionality for tap particles

--- core/src/fi/tuni/tamk/tiko/depressionaut/GameScreen.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/GameScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.ArrayList;
--- core/src/fi/tuni/tamk/tiko/depressionaut/TapParticle.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/TapParticle.java
@@ -2,45 +2,58 @@ package fi.tuni.tamk.tiko.depressionaut;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class TapParticle {
     private Texture particleTexture = new Texture("visuals.png");;
     private ArrayList<TapParticle> Particles = new ArrayList<>();
 
+    Random random = new SecureRandom();
+
     private float x;
     private float y;
+    private Vector2 vector;
+    private float speed = 5;
 
-    private float width;
-    private float height;
     private float scale;
     private float rotation;
 
-    public TapParticle() {
-
-    }
+    public TapParticle() {}
 
     public void createParticle() {
         Particles.add(new TapParticle());
 
-        Particles.get(Particles.size()-1).setX(50);
-        Particles.get(Particles.size()-1).setY(50);
-        Particles.get(Particles.size()-1).width = 500;
-        Particles.get(Particles.size()-1).height = 500;
-        Particles.get(Particles.size()-1).scale = 1;
-        Particles.get(Particles.size()-1).rotation = 0;
+        // Set temp particle:
+        TapParticle tempParticle = Particles.get(Particles.size()-1);
+
+        tempParticle.scale = randomFloat(0.75f, 1.25f);
+        tempParticle.rotation = randomFloat(-30, 150);
+
+        tempParticle.vector = new Vector2((float)Math.cos(Math.toRadians(Particles.get(Particles.size()-1).rotation + 90)) * speed,
+                (float)Math.sin(Math.toRadians(Particles.get(Particles.size()-1).rotation + 90)) * speed);
+
+        tempParticle.setX(getHeadX() - (particleTexture.getWidth() / 2f) + tempParticle.vector.x * 50);
+        tempParticle.setY(getHeadY() - (particleTexture.getHeight() / 2f) + tempParticle.vector.y * 50);
+
+        // Set real particle:
+        Particles.set(Particles.size()-1, tempParticle);
     }
 
     public void renderParticles(SpriteBatch batch) {
         for (TapParticle p : Particles) {
+            moveParticle(p);
+
             batch.draw(particleTexture,
                     p.getX(),
                     p.getY(),
-                    p.width / 2,
-                    p.height / 2,
-                    p.width,
-                    p.height,
+                    particleTexture.getWidth() / 2f,
+                    particleTexture.getHeight() / 2f,
+                    particleTexture.getWidth(),
+                    particleTexture.getHeight(),
                     p.scale,
                     p.scale,
                     p.rotation,
@@ -51,6 +64,23 @@ public class TapParticle {
                     false,
                     false);
         }
+    }
+
+    public void moveParticle(TapParticle p) {
+        p.setX(p.getX() + p.vector.x);
+        p.setY(p.getY() + p.vector.y);
+    }
+
+    // TODO: Move to character's own class:
+    public float getHeadX() {
+        return 740f;
+    }
+    public float getHeadY() {
+        return 1920 - 1070f;
+    }
+
+    public float randomFloat(float min, float max) {
+        return min + random.nextFloat() * (max - min);
     }
 
     public void setX(float x) {
