1b1209dfa9a097f61473be20924628eba655092a
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-03-24 15:15:45 +0200

    rename navigation

--- core/src/fi/tuni/tamk/tiko/depressionaut/Navigation.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/Navigation.java
@@ -4,45 +4,61 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
-public class BottomHud {
+public class Navigation {
+    private Texture ui;
     private Texture game = new Texture("UI/gamePressed.png");
     private Texture shop = new Texture("UI/shopPressed.png");
     private Texture settings = new Texture("UI/settingsPressed.png");
 
-    private int hudSelection = 0;
+    private Screen hudSelection;
+
+    public enum Screen {
+        NONE(0),
+        GAME(1),
+        SHOP(2),
+        SETTINGS(3);
+
+        private final int screenIndex;
+
+        Screen(int screenIndex) {
+            this.screenIndex = screenIndex;
+        }
+
+        public int getScreenIndex() {
+            return screenIndex;
+        }
+    };
+
+    public Navigation() {
+        ui = new Texture("UI/gamePressed.png");
+        current = Screen.GAME;
+    }
+
 
 
     /**
      * Draws the hud that is selected by a number 0-4
      *
-     * 0 = No HUD
-     * 1 = Game HUD
-     * 2 = Shop HUD
-     * 3 = Settings HUD
      * @param batch
      */
     public void draw(SpriteBatch batch) {
 
-        if(hudSelection == 1) {
+        if(hudSelection == Screen.GAME) {
             batch.draw(game, 0, 0);
         }
-        if(hudSelection == 2) {
+        if(hudSelection == Screen.SHOP) {
             batch.draw(shop, 0, 0);
         }
-        if(hudSelection == 3) {
+        if(hudSelection == Screen.SETTINGS) {
             batch.draw(settings, 0, 0);
         }
 
     }
 
     /**
-     * 0 = No HUD
-     * 1 = Game HUD
-     * 2 = Shop HUD
-     * 3 = Settings HUD
      * @param hudSelection
      */
-    public void setHudSelection(int hudSelection) {
+    public void setHudSelection(Screen hudSelection) {
         this.hudSelection = hudSelection;
     }
 }
