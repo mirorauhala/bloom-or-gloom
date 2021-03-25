2f74657c108e97cae526b739b00313c402bf60f6
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-03-25 02:43:41 +0200

    add settings screen overrides

--- core/src/fi/tuni/tamk/tiko/depressionaut/SettingsScreen.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/SettingsScreen.java
@@ -1,10 +1,75 @@
 package fi.tuni.tamk.tiko.depressionaut;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 
-public class SettingsScreen {
+public class SettingsScreen implements Screen {
+    private MyGdxGame game;
 
-    public void draw(SpriteBatch batch) {
+    public SettingsScreen(MyGdxGame game) {
+        this.game = game;
+    }
+
+    /**
+     * Called when this screen becomes the current screen for a {@link Game}.
+     */
+    @Override
+    public void show() {
+
+    }
+
+    /**
+     * Called when the screen should render itself.
+     *
+     * @param delta The time in seconds since the last render.
+     */
+    @Override
+    public void render(float delta) {
+        // set background to white
+        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    /**
+     * @param width
+     * @param height
+     * @see ApplicationListener#resize(int, int)
+     */
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    /**
+     * @see ApplicationListener#pause()
+     */
+    @Override
+    public void pause() {
+
+    }
+
+    /**
+     * @see ApplicationListener#resume()
+     */
+    @Override
+    public void resume() {
+
+    }
+
+    /**
+     * Called when this screen is no longer the current screen for a {@link Game}.
+     */
+    @Override
+    public void hide() {
+
+    }
+
+    /**
+     * Called when this screen should release all resources.
+     */
+    @Override
+    public void dispose() {
 
     }
 }
