e9408ed5a2193d4273922d6818a3020021f2acb1
jerejoensuu <jere.joensuu@tuni.fi>
2021-03-26 19:44:57 +0200

    Auto stash before rebase of "refs/heads/Character"

---
+++ core/src/fi/tuni/tamk/tiko/depressionaut/GameCharacter.java
@@ -0,0 +1,17 @@
+package fi.tuni.tamk.tiko.depressionaut;
+
+/*
+    head offset values:
+    - tier 1: down 65px
+    - tier 2: down 15px
+ */
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameCharacter {
+
+    public static void draw(SpriteBatch batch) {
+
+    }
+
+}
