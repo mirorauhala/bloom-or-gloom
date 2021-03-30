0eb36b14f8c55ea8156e751b2e4771d856206cff
jerejoensuu <jere.joensuu@tuni.fi>
2021-03-30 11:17:19 +0300

    Make mood meter filling smoother

--- core/src/fi/tuni/tamk/tiko/depressionaut/ScoreMeter.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/ScoreMeter.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,27 +24,12 @@ public class ScoreMeter {
     public Texture meterBase = new Texture("UI/UpperHUDbase.png");
     public Texture meterTexture;
 
+    private ArrayList<Rectangle> rectangles = new ArrayList<Rectangle>();
+    private Rectangle meterFiller;
+    private Texture meter;
+    private Texture meterColor;
+
     public BitmapFont font;
-
-
-    /**
-     * meterTextures houses all the increments for the happiness meter
-     */
-    public List<Texture> meterTextures = Arrays.asList(
-            new Texture("UI/Meters/Meter0.png"),
-            new Texture("UI/Meters/Meter1.png"),
-            new Texture("UI/Meters/Meter2.png"),
-            new Texture("UI/Meters/Meter3.png"),
-            new Texture("UI/Meters/Meter4.png"),
-            new Texture("UI/Meters/Meter5.png"),
-            new Texture("UI/Meters/Meter6.png"),
-            new Texture("UI/Meters/Meter7.png"),
-            new Texture("UI/Meters/Meter8.png")
-    );
-
-    public void setMeterTexture(int amountOfBarsInMeter) {
-        meterTexture = meterTextures.get(amountOfBarsInMeter);
-    }
 
     public Texture getMeterTexture() {
         return meterTexture;
@@ -53,6 +40,7 @@ public class ScoreMeter {
      *
      * will get changed when the happiness level is invented
      */
+    /*
     public void determineMeterHeight() {
         int amountOfScoreForNextLevel = 0;
 
@@ -101,22 +89,58 @@ public class ScoreMeter {
         }
     }
 
+     */
+
     public ScoreMeter(MyGdxGame game) {
         this.game = game;
         this.camera = game.camera;
         this.score = game.score;
         font = new BitmapFont(Gdx.files.internal("font/Quicksand.fnt"));
         font.setUseIntegerPositions(false);
+
+        meterFiller = new Rectangle();
+        meter = new Texture("UI/meter.png");
+        meterColor = new Texture("UI/meterColor.png");
+        createFillerRectangles();
     }
 
     public void draw(SpriteBatch batch) {
-        determineMeterHeight();
+        //determineMeterHeight();
+
         batch.draw(meterBase, 0, 0);
-        batch.draw(meterTexture, 0, 0);
+        for (int i = 0; i <= meter.getHeight() * getProgressToNextTier(); i++) {
+            batch.draw(meterColor,
+                    rectangles.get(i).getX(),
+                    rectangles.get(i).getY(),
+                    rectangles.get(i).getWidth(),
+                    rectangles.get(i).getHeight());
+        }
+        batch.draw(meter, 22, 1920 - 358, meter.getWidth(), meter.getHeight());
         font.getData().setScale(3f, 3f);
         font.draw(batch, score.getRationalizedScore(), 520, 1870);
         font.getData().setScale(2f, 2f);
         font.draw(batch, "e/s " + score.getPassiveIncome(), 410, 1740);
 
+    }
+
+    public float getProgressToNextTier() {
+        return score.getScore() / score.getScoreForNextLevel(score.getHappinessLevel());
+    }
+
+    public float getFillerWidth(int index) {
+        // calculate distance to the center of the circle:
+        float d = Math.abs(meter.getHeight()/2 - index);
+        return (float) (2 * Math.sqrt(Math.pow(meter.getWidth()/2f, 2) - Math.pow(d, 2)));
+    }
+
+    public void createFillerRectangles() {
+        for (int i = 0; i <= meter.getHeight(); i++) {
+            rectangles.add(new Rectangle(
+                    22 + (meter.getWidth() - getFillerWidth(i))/2f,
+                    1920 - 358 + i,
+                    getFillerWidth(i),
+                    1
+            ));
+        }
     }
 }
