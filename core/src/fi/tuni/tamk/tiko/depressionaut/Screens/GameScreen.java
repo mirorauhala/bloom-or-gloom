c3b5bedbb07a8697c2cae2351db1cd8c05a7d675
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-10 20:37:41 +0300

    read current tiers from inventory

--- core/src/fi/tuni/tamk/tiko/depressionaut/Screens/GameScreen.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/Screens/GameScreen.java
@@ -232,6 +232,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
+        applyCurrentTierTextures();
     }
 
     @Override
@@ -257,5 +258,19 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+
+    /**
+     * Fetches the current tiers for all textures from the Inventory.
+     */
+    private void applyCurrentTierTextures() {
+        setWallTier(game.inventory.get("wall"));
+        setFloorTier(game.inventory.get("wall"));
+        setBedTier(game.inventory.get("wall"));
+        setChairTier(game.inventory.get("wall"));
+        setCharacterTier(game.inventory.get("wall"));
+        setDeskTier(game.inventory.get("wall"));
+        setSmileTier(game.inventory.get("wall"));
     }
 }
