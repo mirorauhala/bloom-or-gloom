15e227186b71f2249991dcaa65d747b11f972fc7
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-11 18:16:42 +0300

    fix crash on android

--- core/src/fi/tuni/tamk/tiko/depressionaut/Screens/ShopScreen.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/Screens/ShopScreen.java
@@ -33,8 +33,8 @@ public class ShopScreen implements Screen {
     public ShopScreen(final MyGdxGame game){
         this.game = game;
 
-        FileHandle handle = Gdx.files.local("shop/products.json");
         text = handle.readString();
+        FileHandle handle = Gdx.files.internal("shop/products.json");
 
         Gson gson = new Gson();
         Products products = gson.fromJson(text, Products.class);
