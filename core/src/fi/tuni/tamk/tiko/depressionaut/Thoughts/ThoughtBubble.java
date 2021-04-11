5f8bfa5338cfae0c4af7fda6f8d2ba4202a96119
jerejoensuu <jere.joensuu@tuni.fi>
2021-04-11 21:07:56 +0300

    test

--- core/src/fi/tuni/tamk/tiko/depressionaut/Thoughts/ThoughtBubble.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/Thoughts/ThoughtBubble.java
@@ -98,6 +98,7 @@ public class ThoughtBubble extends Actor {
                             false,
                             false);
                 }
+                // kommentin
             }
         }
     }
