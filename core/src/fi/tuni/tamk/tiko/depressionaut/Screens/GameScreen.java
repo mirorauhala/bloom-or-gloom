44fee59a728c9c776878a3564708be28b5abbb48
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-11 18:39:54 +0300

    Revert "Revert "read current tiers from inventory""
    This reverts commit f60641d8d3b4a1315625bf653f4480c1a434f264.

--- core/src/fi/tuni/tamk/tiko/depressionaut/Screens/GameScreen.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/Screens/GameScreen.java
@@ -226,6 +226,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void show() {
+        applyCurrentTierTextures();
     }
 
     @Override
@@ -251,5 +252,19 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+
+    /**
+     * Fetches the current tiers for all textures from the Inventory.
+     */
+    private void applyCurrentTierTextures() {
+        setWallTier(game.inventory.get("wall"));
+        setFloorTier(game.inventory.get("wall"));
+        setBedTier(game.inventory.get("wall"));
+        setChairTier(game.inventory.get("wall"));
+        setCharacterTier(game.inventory.get("wall"));
+        setDeskTier(game.inventory.get("wall"));
+        setSmileTier(game.inventory.get("wall"));
     }
 }
