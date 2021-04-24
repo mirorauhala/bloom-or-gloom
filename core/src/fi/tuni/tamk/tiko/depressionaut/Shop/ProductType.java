ef5a6a9af35e4ae3956cf6da34b37e710b9936c1
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-24 19:59:06 +0300

    rename Type to match others

---
+++ core/src/fi/tuni/tamk/tiko/depressionaut/Shop/ProductType.java
@@ -0,0 +1,14 @@
+package fi.tuni.tamk.tiko.depressionaut.Shop;
+
+public class ProductType {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
