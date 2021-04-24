effb629f719c86abd4be00b6ad74372cd48c2989
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2021-04-24 19:59:40 +0300

    add javadoc

--- core/src/fi/tuni/tamk/tiko/depressionaut/Shop/Product.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/Shop/Product.java
@@ -2,6 +2,9 @@ package fi.tuni.tamk.tiko.depressionaut.Shop;
 
 import java.util.List;
 
+/**
+ * A class representing a single product. Serialized to a POJO from JSON using google/gson.
+ */
 public class Product {
     private int id;
     private String name;
@@ -11,30 +14,58 @@ public class Product {
     private String texture;
     private List<ProductEffect> effects;
 
+    /**
+     * Returns the product's id.
+     * @return int
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Returns the product's name.
+     * @return String
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Return the product's name.
+     * @return String
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Returns the product's level.
+     * @return int
+     */
     public int getLevel() {
         return level;
     }
 
+    /**
+     * Returns the product's texture.
+     * @return int
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * Returns the product's texture.
+     * @return String product texture
+     */
     public String getTexture() {
         return texture;
     }
 
+    /**
+     * Returns the product's effects.
+     * @return List<ProductEffect>
+     */
     public List<ProductEffect> getEffects() {
         return effects;
     }
--- core/src/fi/tuni/tamk/tiko/depressionaut/Shop/Products.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/Shop/Products.java
@@ -2,6 +2,9 @@ package fi.tuni.tamk.tiko.depressionaut.Shop;
 
 import java.util.List;
 
+/**
+ * A class representing a collection of products. Serialized to a POJO from JSON using google/gson.
+ */
 public class Products {
     private List<Product> products;
     private List<ProductType> types;
