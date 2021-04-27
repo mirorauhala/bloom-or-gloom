e5c560eb0fd47abbe7ce5d3b7cfb0bc3792c6b64
Jere <jere.joensuu@tuni.fi>
2021-04-27 14:13:57 +0300

    Round score to three decimals

--- core/src/fi/tuni/tamk/tiko/depressionaut/Util.java
+++ core/src/fi/tuni/tamk/tiko/depressionaut/Util.java
@@ -1,5 +1,7 @@
 package fi.tuni.tamk.tiko.depressionaut;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -16,5 +18,20 @@ public class Util {
     public static float randomFloat(float min, float max) {
         Random random = new SecureRandom();
         return min + random.nextFloat() * (max - min);
+    }
+
+    /**
+     * Rounds a double to specified decimals.
+     *
+     * @param value Given double
+     * @param places Desired number of decimals
+     * @return Rounded double
+     */
+    public static double roundDouble(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
